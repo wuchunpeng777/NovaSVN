990db3149a8c39af7ff5ff1f18c81b05d8046a53
wuchunpeng777 <wcpwcp9@gmail.com>
2026-07-03 22:16:21 +0800

    优化工作台视觉风格

--- src/components/workbench/Toolbar.svelte
+++ src/components/workbench/Toolbar.svelte
@@ -11,7 +11,7 @@
     <p>{subtitle}</p>
   </div>
   <div class="toolbar-actions">
-    <button type="button" on:click={onPing}>连接后端</button>
+    <button type="button" on:click={onPing}>后端自检</button>
     <button type="button" on:click={onPreviewError}>验证错误</button>
   </div>
 </header>
